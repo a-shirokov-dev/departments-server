8bcc5a073381b5bab68a15e53145a932d8c40d41
a-shirokov-exceed <ashirokov.exceedteam@gmail.com>
2021-12-03 16:15:30 +0300

    Fix for Joi validation schema

--- src/modules/middleware/middleware.js
+++ src/modules/middleware/middleware.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 module.exports.departmentValidation = async (req, _res, next) => {
   const schema = Joi.object({
     name: Joi.string().max(30).required(),
-    description: Joi.string(),
+    description: Joi.string().allow(''),
   });
   validationBody(schema, req, next);
 };
@@ -24,7 +24,6 @@ const validationBody = (schema, req, next) => {
     allowUnknown: true,
     stripUnknown: true,
   };
-
   const { error } = schema.validate(req.body, options);
   if (error) {
     next(`Validation error: ${error.details.map((x) => x.message).join(", ")}`);
